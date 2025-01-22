6f915ad3ece5dc37d185282b063911e5e6026a08
Omar <pro_omrlock@outlook.com>
2025-01-23 00:17:30 +0200

    finished the articles component

--- src/app/(home)/_components/articles-slider.tsx
+++ src/app/(home)/_components/articles-slider.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 type Article = {
   id: string;
@@ -17,28 +18,67 @@ type Article = {
 };
 
 export default function ArticlesSlider({ articles }: { articles: Article[] }) {
-  function handleArrowClick(type: "prev" | "next") {
-    console.log(type);
-    switch (type) {
-      case "prev":
-        break;
-      case "next":
-        break;
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const [cardWidth, setCardWidth] = useState("1px");
+  useEffect(() => {
+    function changeCardWidth() {
+      const windowWidth = window.innerWidth;
+      const paddings = [16, 32, 64, 80];
+      const gap = 16;
+      // const fixedVal = 64;
+      let cardsCount = 1;
+      if (windowWidth >= 1280) cardsCount = 4;
+      else if (windowWidth >= 1024) cardsCount = 3;
+      else if (windowWidth >= 768) cardsCount = 2;
+      const cardWidth =
+        (windowWidth -
+          paddings[cardsCount - 1] -
+          cardsCount * gap -
+          (paddings[cardsCount - 1] - 16)) /
+        cardsCount;
+      setCardWidth(cardWidth + "px");
     }
-  }
+
+    window.addEventListener("resize", changeCardWidth);
+    changeCardWidth();
+    return () => {
+      window.removeEventListener("resize", changeCardWidth);
+    };
+  }, []);
+
+  const scrollByOneSnap = (direction: "left" | "right") => {
+    if (!sliderRef.current) return;
+    const slider = sliderRef.current;
+    const cardWidth = slider.children[0]?.clientWidth || 0;
+    const scrollAmount = direction === "left" ? -cardWidth : cardWidth;
+    slider.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div>
-      <div className="flex gap-4">
+      <div
+        ref={sliderRef}
+        className="px-cont-sm md:px-cont-md lg:px-cont-lg xl:px-cont-xl flex snap-x snap-mandatory scroll-px-4 gap-4 overflow-x-auto pb-5 pt-4 *:snap-start md:scroll-px-8 lg:scroll-px-16 xl:scroll-px-20"
+      >
         {articles.map((article) => (
-          <ArticleCard key={article.id} {...article} />
+          <ArticleCard key={article.id} {...article} cardWidth={cardWidth} />
         ))}
       </div>
+      {/* arrows */}
       <div className="flex justify-center gap-3">
-        <button onClick={() => handleArrowClick("prev")}>
+        <button
+          onClick={() => scrollByOneSnap("left")}
+          className="transition-transform hover:scale-[1.1]"
+        >
           <MdKeyboardArrowLeft size={50} />
         </button>
-        <button onClick={() => handleArrowClick("next")}>
+        <button
+          onClick={() => scrollByOneSnap("right")}
+          className="transition-transform hover:scale-[1.1]"
+        >
           <MdKeyboardArrowRight size={50} />
         </button>
       </div>
@@ -46,10 +86,20 @@ export default function ArticlesSlider({ articles }: { articles: Article[] }) {
   );
 }
 
-function ArticleCard({ title, author, readTime, imageURL, link }: Article) {
+function ArticleCard({
+  title,
+  author,
+  readTime,
+  imageURL,
+  link,
+  cardWidth,
+}: Article & { cardWidth: string }) {
   return (
-    <article className="group aspect-[4/5] flex-1 overflow-hidden rounded-2xl bg-slate-200 transition-all hover:scale-[1.03] hover:shadow-xl">
-      <Link href={`/article/${link}`} className="flex h-full flex-col">
+    <article style={{ minWidth: cardWidth }} className={``}>
+      <Link
+        href={`/article/${link}`}
+        className="group flex aspect-[4/5] h-full flex-col overflow-hidden rounded-2xl bg-[#F5F5E4] transition-all hover:scale-[1.03] hover:shadow-xl"
+      >
         <Image
           src={imageURL}
           alt="article image"
@@ -58,7 +108,7 @@ function ArticleCard({ title, author, readTime, imageURL, link }: Article) {
           className="aspect-[2] w-full object-cover"
         />
         <div className="flex grow flex-col justify-between gap-5 p-4">
-          <div className="">
+          <div className="flex flex-col gap-3">
             <h4>{title}</h4>
             <p className="font-semibold">{author}</p>
           </div>
