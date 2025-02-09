e77e0f2822a9b36dde7c0e14d4ebf75d9d609732
Omar <pro_omrlock@outlook.com>
2025-02-09 13:46:09 +0200

    added favicon

---
+++ src/app/head.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
+      <link
+        rel="icon"
+        href="/icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
+    </>
+  );
+}
